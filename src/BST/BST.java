a6c9bd6ca023942af7a0f0269f9e64fe09ed623d
alisherkin <77731269+alisherkin@users.noreply.github.com>
2021-05-24 04:27:50 +0600

    Update BST.java

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -1,7 +1,5 @@
 package BST;
 
-import java.util.Stack;
-
 public class BST<K extends Comparable<? super K>, V> {
 
     public Node root;
@@ -138,44 +136,6 @@ public class BST<K extends Comparable<? super K>, V> {
         //if no both child?
     }
 
-    public void print() { // метод для вывода дерева в консоль
-        Stack globalStack = new Stack(); // общий стек для значений дерева
-        globalStack.push(root);
-        int gaps = 32; // начаное значение расстояния между элементами
-        boolean isRowEmpty = false;
-        String separator = "-----------------------------------------------------------------";
-        System.out.println(separator);// черта для указания начала нового дерева
-        while (isRowEmpty == false) {
-            Stack localStack = new Stack(); // локальный стек для задания потомков элемента
-            isRowEmpty = true;
-
-            for (int j = 0; j < gaps; j++)
-                System.out.print(' ');
-            while (globalStack.isEmpty() == false) { // покуда в общем стеке есть элементы
-                Node temp = (Node) globalStack.pop(); // берем следующий, при этом удаляя его из стека
-                if (temp != null) {
-                    System.out.print(temp.value); // выводим его значение в консоли
-                    localStack.push(temp.left); // соохраняем в локальный стек, наследники текущего элемента
-                    localStack.push(temp.right);
-                    if (temp.left != null ||
-                            temp.right != null)
-                        isRowEmpty = false;
-                }
-                else {
-                    System.out.print("__");// - если элемент пустой
-                    localStack.push(null);
-                    localStack.push(null);
-                }
-                for (int j = 0; j < gaps * 2 - 2; j++)
-                    System.out.print(' ');
-            }
-            System.out.println();
-            gaps /= 2;// при переходе на следующий уровень расстояние между элементами каждый раз уменьшается
-            while (localStack.isEmpty() == false)
-                globalStack.push(localStack.pop()); // перемещаем все элементы из локального стека в глобальный
-        }
-        System.out.println(separator);// подводим черту
-    }
 
     public void printTree(Node root) {
         if(root!=null){
